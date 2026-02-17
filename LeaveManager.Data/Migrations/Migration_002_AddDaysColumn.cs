bb0f3df657bfe56a98309191a1d6c0ab2d7e9e31
ayberk-kara <ayberkkara4042@gmail.com>
2026-02-17 17:08:36 +0300

    add days column migration

---
+++ LeaveManager.Data/Migrations/Migration_002_AddDaysColumn.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace LeaveManager.Data.Migrations
+{
+    public sealed class Migration_002_AddDaysColumn : IMigration
+    {
+        public int Version => 2;
+
+        public void Up(SqliteConnection connection)
+        {
+            using var cmd = connection.CreateCommand();
+
+            cmd.CommandText = @"
+ALTER TABLE Leaves
+ADD COLUMN days INTEGER NOT NULL DEFAULT 0;
+";
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
