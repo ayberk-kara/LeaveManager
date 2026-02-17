f2c49b1d80d1fe42d88049c38c5e85c67e57a7a2
ayberk-kara <ayberkkara4042@gmail.com>
2026-02-17 16:06:16 +0300

    add leaveService

---
+++ src/LeaveManager.App/LeaveService.cs
@@ -0,0 +1,50 @@
+﻿using LeaveManager.App;
+using LeaveManager.Data.Models;
+using LeaveManager.Data.Repositories;
+using System;
+using System.Collections.Generic;
+
+namespace LeaveManager.Business
+{
+    public class LeaveService
+    {
+        private readonly LeaveRepository _leaveRepository;
+        private readonly EmployeeRepository _employeeRepository;
+        private readonly List<LeaveRule> _rules;
+
+        public LeaveService()
+        {
+            _leaveRepository = new LeaveRepository();
+            _employeeRepository = new EmployeeRepository();
+
+            _rules = new List<LeaveRule>
+            {
+                new DateRangeRule(),
+                new NoPastStartRule(),
+                new MaxConsecutiveDaysRule(),
+                new NoOverlapRule(),
+                new LongLeaveGapRule(),
+                new AnnualLeaveLimitRule(),
+                new SickLeaveLimitRule()
+            };
+        }
+
+        public bool TryAddLeave(Leave newLeave, out string errorMessage)
+        {
+            var employee = _employeeRepository.GetById(newLeave.EmployeeId);
+            var existingLeaves = _leaveRepository.GetByEmployeeId(newLeave.EmployeeId);
+            var allEmployees = _employeeRepository.GetAll();
+
+            foreach (var rule in _rules)
+            {
+                if (!rule.Validate(employee, allEmployees, existingLeaves, newLeave, out errorMessage))
+                    return false;
+            }
+
+            _leaveRepository.Add(newLeave);
+
+            errorMessage = string.Empty;
+            return true;
+        }
+    }
+}
