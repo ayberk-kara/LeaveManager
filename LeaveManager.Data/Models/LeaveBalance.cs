e8afcfaa0aa24e65a7d19b782a16f00a4aee431a
ayberk-kara <ayberkkara4042@gmail.com>
2026-02-17 18:57:41 +0300

    leave balance

---
+++ LeaveManager.Data/Models/LeaveBalance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeaveManager.Data.Models
+{
+    public sealed class LeaveBalance
+    {
+        public int Id { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public int Year { get; set; }
+
+        // Annual
+        public int AnnualEntitled { get; set; } = 30;
+        public int AnnualUsed { get; set; } = 0;
+        public int AnnualManualAdjust { get; set; } = 0;
+
+        // Sick
+        public int SickEntitled { get; set; } = 40;
+        public int SickUsed { get; set; } = 0;
+        public int SickManualAdjust { get; set; } = 0;
+
+        // Computed helpers
+        public int AnnualRemaining =>
+            AnnualEntitled + AnnualManualAdjust - AnnualUsed;
+
+        public int SickRemaining =>
+            SickEntitled + SickManualAdjust - SickUsed;
+    }
+}
