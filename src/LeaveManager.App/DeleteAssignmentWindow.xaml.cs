ea4f2f0a14c111fe38ab57c1f311a5ef481d9fa8
ayberk-kara <ayberkkara4042@gmail.com>
2026-03-07 19:52:16 +0300

    add delete assignment confirm window

---
+++ src/LeaveManager.App/DeleteAssignmentWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace LeaveManager.App
+{
+    public partial class DeleteAssignmentWindow : Window
+    {
+        public DeleteAssignmentWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Confirm_Click(object sender, RoutedEventArgs e)
+        {
+            
+            DialogResult = true;
+            this.Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            
+            DialogResult = false;
+            this.Close();
+        }
+    }
+}
