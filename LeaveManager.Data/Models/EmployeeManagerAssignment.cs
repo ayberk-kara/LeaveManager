bc49820bbfb2b66c05960526045f5f01d7efe7af
ayberk-kara <ayberkkara4042@gmail.com>
2026-03-07 13:29:47 +0300

    update new table

--- LeaveManager.Data/Models/EmployeeManagerAssignment.cs
+++ LeaveManager.Data/Models/EmployeeManagerAssignment.cs
@@ -8,8 +8,8 @@
 
         public int ManagerId { get; set; }
 
-        public DateTime StartDate { get; set; }
+        public int Year { get; set; }
 
-        public DateTime EndDate { get; set; }
+        public int Month { get; set; }
     }
 }
