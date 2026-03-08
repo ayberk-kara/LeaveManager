dd88402e6533a424417932df1cb4f2f5ece40f7c
ayberk-kara <ayberkkara4042@gmail.com>
2026-03-08 18:34:06 +0300

    add year selection backend

--- src/LeaveManager.App/YearSelectionWindow.xaml.cs
+++ src/LeaveManager.App/YearSelectionWindow.xaml.cs
@@ -1,27 +1,46 @@
-﻿using System;
+﻿using LeaveManager.Data.Repositories;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-namespace IzinProgrami
+namespace LeaveManager.App
 {
-    /// <summary>
-    /// Interaction logic for YearSelectionWindow.xaml
-    /// </summary>
     public partial class YearSelectionWindow : Window
     {
+        public int? SelectedYear { get; private set; }
+
         public YearSelectionWindow()
         {
             InitializeComponent();
+            LoadYears();
+        }
+
+        private void LoadYears()
+        {
+            var leaveRepository = new LeaveRepository();
+            List<int> years = leaveRepository.GetYearsWithLeaves(); 
+            years.Sort();
+            cmbYear.ItemsSource = years;
+            if (years.Any())
+                cmbYear.SelectedIndex = 0;
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (cmbYear.SelectedItem != null)
+            {
+                SelectedYear = (int)cmbYear.SelectedItem;
+                DialogResult = true;
+            }
+            else
+            {
+                MessageBox.Show("Lütfen bir yıl seçin.", "Uyarı", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
         }
     }
 }
